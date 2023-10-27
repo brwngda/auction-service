6d3b809d83f205dd463cb204614998e33373908c
brwngda <grz.barwinski@gmail.com>
2023-10-27 18:40:06 +0200

    add missing annotation to CorsConfig

--- src/main/java/com/example/auctionservice/config/CorsConfig.java
+++ src/main/java/com/example/auctionservice/config/CorsConfig.java
@@ -1,8 +1,9 @@
 package com.example.auctionservice.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
+@Configuration
 class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
